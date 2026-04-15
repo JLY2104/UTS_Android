9abf91a5ebe9185305ff267a4ad133d456f934fe
JLY2104 <williesfirnanda149@gmail.com>
2026-04-15 15:33:17 +0700

    menambahkan gradle dan membuat database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,9 @@ android {
 }
 
 dependencies {
+    val  room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:2.8.4")
+    implementation("androidx.room:room-ktx:2.8.4")
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.compose.material3:material3")
---
+++ app/src/main/java/com/example/unscramble/data/WordDao.kt
@@ -0,0 +1,4 @@
+package com.example.unscramble.data
+
+import androidx.room.Dao
+import  androidx.room.I
